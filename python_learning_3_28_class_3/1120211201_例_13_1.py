041e306538e508077c158768e2c83ae46ae963da
Cheng81112 <wangcheng981112@163.com>
2022-04-04 09:05:19 +0800

    python课程作业

--- python_learning_3_28_class_3/1120211201_例_13_1.py
+++ python_learning_3_28_class_3/1120211201_例_13_1.py
@@ -1,4 +1,11 @@
 '''
-
+根窗体
 '''
-import tkinter
+from tkinter import *
+
+root = Tk()  # 创建一个根窗体，第一个T大写，第二个k小写
+
+root.title("我的第一个python窗体")
+root.geometry("320x240")  # 注意中间是小写的字母x表示乘
+root.mainloop()
+
